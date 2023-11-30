68db2beefd8d3683d3b967467e1ca2d48b382830
Albert Jie <albertjie@easyops.cn>
2023-11-30 18:45:07 +0800

    增加tabBar

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,4 +1,5 @@
 import { CLOUD_ID } from './config/config';
+import { SESSION_KEY } from './constants';
 
 App({
   onLaunch: function () {
@@ -6,13 +7,27 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
         env: CLOUD_ID,
         traceUser: true,
       });
+
+      this.IsLogon();
     }
+  },
+
+  IsLogon() {
+    const userInfo = wx.getStorageSync(SESSION_KEY);
+
+    if (userInfo.name && userInfo.phone) {
+        this.globalData.isLogged = true
+        this.globalData.userInfo = userInfo
+    } else {
+        this.globalData.isLogged = false
+    }
+  },
+
+  globalData: {
+    userInfo: null,
+    isLogged: false
   }
 });
